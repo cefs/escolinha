0d31974b7f0f4718c7425d47c1fa2c931784ed1c
CARLOS SILVA <cefs1984@gmail.com>
2013-03-04 11:34:37 -0300

    order by in search courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -13,7 +13,7 @@ class Course < ActiveRecord::Base
    def self.search(query,page)
       if query.present?         
          joins("LEFT JOIN rooms on rooms.course_id = courses.id LEFT JOIN teachers on rooms.teacher_id = teachers.id").         
-         where(["courses.name #{AdapterSpecific.like_case_insensitive} :query OR teachers.name #{AdapterSpecific.like_case_insensitive} :query", :query => "%#{query}%"]).group("courses.id")
+         where(["courses.name #{AdapterSpecific.like_case_insensitive} :query OR teachers.name #{AdapterSpecific.like_case_insensitive} :query", :query => "%#{query}%"]).group("courses.id").order("courses.name")
       else         
          paginate :page => page, :size => 8
       end
